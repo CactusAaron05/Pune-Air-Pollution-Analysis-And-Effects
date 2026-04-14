784053d54a1a80324026c4690b73bd0a32fb12a7
CactusAaron05 <aaronjdias2005@gmail.com>
2026-04-14 18:52:40 +0530

    Updated db.py

--- backend/db.py
+++ backend/db.py
@@ -3,7 +3,7 @@ import psycopg2
 def get_connection():
     return psycopg2.connect(
         host="localhost",
-        database="pune_aqi",
-        user="postgres",
-        password="mubauman@3"
+        database="your_databse",
+        user="your_user",
+        password="your_password"
     )
